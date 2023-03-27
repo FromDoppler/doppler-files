dd4e6326622505233a64ec0b0ee071a4977270d5
jpetrozzi-makingsense <jpetrozzi@makingsense.com>
2023-03-27 21:43:17 +0200

    Add Doppler security

---
+++ Doppler.Files.Api/DopplerSecurity/DopplerSecurityOptions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace Doppler.Files.Api.DopplerSecurity;
+
+public class DopplerSecurityOptions
+{
+    public IEnumerable<SecurityKey> SigningKeys { get; set; } = Array.Empty<SecurityKey>();
+}
