668ff409527b63693b2848dc80a6256b5dadd527
Javier Petrozzi <87434557+jpetrozzi-makingsense@users.noreply.github.com>
2023-03-28 19:39:38 +0200

    Add Doppler security (#395)
    * Add Doppler security
    
    * Add private keys

---
+++ Doppler.Files.Api/DopplerSecurity/DopplerSecurityDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace Doppler.Files.Api.DopplerSecurity;
+
+public static class DopplerSecurityDefaults
+{
+    public const string PublicKeysFolderConfigKey = "DopplerSecurity:PublicKeysFolder";
+    public const string PublicKeysFolderDefaultConfigValue = "public-keys";
+    public const string PublicKeysFilenameConfigKey = @"DopplerSecurity:PublicKeysFilenameRegex";
+    public const string PublicKeysFilenameRegexDefaultConfigValue = "\\.xml$";
+    public const string SuperUserJwtKey = "isSU";
+}
--- Doppler.Files.Api/Program.cs
+++ Doppler.Files.Api/Program.cs
@@ -1,7 +1,10 @@
+using Doppler.Files.Api.DopplerSecurity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDopplerSecurity();
 builder.Services.AddControllers();
 
 var app = builder.Build();
