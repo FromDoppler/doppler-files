d756ab16141edde36f88e5efb66c202fedbc9333
jpetrozzi-makingsense <jpetrozzi@makingsense.com>
2023-03-28 22:28:40 +0200

    Add Swagger support

--- Doppler.Files.Api/Program.cs
+++ Doppler.Files.Api/Program.cs
@@ -1,4 +1,5 @@
 using Doppler.Files.Api.DopplerSecurity;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,10 +7,40 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDopplerSecurity();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter the token into field as 'Bearer {token}'",
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme },
+                            },
+                            Array.Empty<string>()
+                        }
+                    });
+
+    var baseUrl = builder.Configuration.GetValue<string>("BaseURL");
+    if (!string.IsNullOrEmpty(baseUrl))
+    {
+        c.AddServer(new OpenApiServer() { Url = baseUrl });
+    };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseStaticFiles();
 
