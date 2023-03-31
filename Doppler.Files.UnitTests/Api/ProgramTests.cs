77533d2013ead33f3929a8eb65680eae4385b596
jpetrozzi-makingsense <jpetrozzi@makingsense.com>
2023-03-31 22:01:58 +0200

    Arrange unit tests

--- Doppler.Files.UnitTests/Api/ProgramTests.cs
+++ Doppler.Files.UnitTests/Api/ProgramTests.cs
@@ -2,24 +2,35 @@
 {
     public class ProgramTests
     {
-        [Fact]
-        public async Task TestSwagger()
+        private readonly WebApplicationFactory<Program> application;
+
+        public ProgramTests()
         {
-            var application = new WebApplicationFactory<Program>();
+            application = new WebApplicationFactory<Program>();
+        }
 
-            var swagger = application.Services
-                                     .GetRequiredService<ISwaggerProvider>()
-                                     .GetSwagger("v1");
-
+        [Theory]
+        [InlineData("/swagger/index.html", HttpStatusCode.OK, "text/html; charset=utf-8")]
+        [InlineData("/swagger/v1/swagger.json", HttpStatusCode.OK, "application/json; charset=utf-8")]
+        public async Task GetEndpoints_ShouldReturnCorrectStatusAndContentType(string url, HttpStatusCode expectedStatusCode, string expectedContentType)
+        {
             var client = application.CreateClient(new WebApplicationFactoryClientOptions()
             {
                 AllowAutoRedirect = false
             });
 
-            var response = await client.GetAsync("/swagger/index.html");
+            var response = await client.GetAsync(url);
 
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.Equal("text/html; charset=utf-8", response.Content?.Headers?.ContentType?.ToString());
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+            Assert.Equal(expectedContentType, response.Content?.Headers?.ContentType?.ToString());
+        }
+        [Fact]
+        public void Swagger_ShouldHaveCorrectSecuritySchema()
+        {
+            var swagger = application.Services
+                                     .GetRequiredService<ISwaggerProvider>()
+                                     .GetSwagger("v1");
+
             Assert.Equal(1, swagger.Components.SecuritySchemes.Count);
             Assert.Equal("Bearer", swagger.Components.SecuritySchemes.Keys.Single());
         }
