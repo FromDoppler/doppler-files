1f92f3a16ef12740f0ef6045b58596d0b56b2bc1
Javier Petrozzi <87434557+jpetrozzi-makingsense@users.noreply.github.com>
2023-04-06 19:51:40 +0200

    Add Swagger support
    * Add Swagger support
    
    * Update package version
    
    * Add Swagger unit test
    
    * Add missing whitespace
    
    * Arrange unit tests
    
    * Add missing new lines
    
    * Add separation line to methods
    
    * Remove redundant reference

---
+++ Doppler.Files.UnitTests/Api/ProgramTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Doppler.Files.UnitTests.Api
+{
+    public class ProgramTests
+    {
+        private readonly WebApplicationFactory<Program> application;
+
+        public ProgramTests()
+        {
+            application = new WebApplicationFactory<Program>();
+        }
+
+        [Theory]
+        [InlineData("/swagger/index.html", HttpStatusCode.OK, "text/html; charset=utf-8")]
+        [InlineData("/swagger/v1/swagger.json", HttpStatusCode.OK, "application/json; charset=utf-8")]
+        public async Task GetEndpoints_ShouldReturnCorrectStatusAndContentType(string url, HttpStatusCode expectedStatusCode, string expectedContentType)
+        {
+            var client = application.CreateClient(new WebApplicationFactoryClientOptions()
+            {
+                AllowAutoRedirect = false
+            });
+
+            var response = await client.GetAsync(url);
+
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+            Assert.Equal(expectedContentType, response.Content?.Headers?.ContentType?.ToString());
+        }
+
+        [Fact]
+        public void Swagger_ShouldHaveCorrectSecuritySchema()
+        {
+            var swagger = application.Services
+                                     .GetRequiredService<ISwaggerProvider>()
+                                     .GetSwagger("v1");
+
+            Assert.Equal(1, swagger.Components.SecuritySchemes.Count);
+            Assert.Equal("Bearer", swagger.Components.SecuritySchemes.Keys.Single());
+        }
+    }
+}
---
+++ Doppler.Files.UnitTests/Usings.cs
@@ -0,0 +1,5 @@
+global using Xunit;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.Extensions.DependencyInjection;
+global using Swashbuckle.AspNetCore.Swagger;
+global using System.Net;
